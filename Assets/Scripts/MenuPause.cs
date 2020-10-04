3d1dad7cd15fadbaf2132133f5d71b84d146f5f4
spirosvg <jonathaafrias@gmail.com>
2020-10-04 18:34:27 -0300

    PAuse

---
+++ Assets/Scripts/MenuPause.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPause : MonoBehaviour
+{
+    [SerializeField] GameObject menu;
+    bool showMenu = false;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (showMenu)
+            {
+                OcultarMenu();
+            }
+            else
+            {
+                MostrarMenu();
+            }
+        }
+    }
+
+    private void Awake()
+    {
+
+
+        menu.SetActive(false);
+    }
+
+    public void MostrarMenu()
+    {
+        showMenu = true;
+        menu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void OcultarMenu()
+    {
+        showMenu = false;
+        menu.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+
+    public void VolverAlMenu()
+    {
+        OcultarMenu();
+       // SceneManager.LoadScene("Menu");
+    }
+}
