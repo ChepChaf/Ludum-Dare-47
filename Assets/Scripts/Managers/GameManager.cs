4ecb03b7670d61c5ca6816eb4e771b8e41abd177
Cezar Azevedo de Faveri <cazevedodefaveri@gmail.com>
2020-10-05 19:34:17 -0300

    Add UI for GameOver and Win

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -27,15 +28,67 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private GameObject menuPause;
 
+    private GameObject player;
     private GameObject trajectory;
     FuncionDeTrayectoria fdet;
 
+    public Text gameOverText;
+    public Text instructionText;
+
+    public GameObject gameFinishedPanel;
+
     public int coinsPicked = 0;
+
+    public bool isGameFinished = false;
 
     private void Awake()
     {
         soundManager = GameObject.Find("SoundManager").GetComponent<SoundManager>();
         menuPause = Instantiate(menuPause);
+    }
+
+    private void Update()
+    {
+        if (isGameOver)
+        {
+            gameOverText.gameObject.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                Reset();
+            }
+        }
+
+        if (isGameFinished)
+        {
+            Time.timeScale = 0f;
+
+            instructionText.gameObject.SetActive(false);
+            gameFinishedPanel.gameObject.SetActive(true);
+        }
+    }
+
+    private void Reset()
+    {
+        gameOverText.gameObject.SetActive(false);
+        if (trajectory != null)
+        {
+            Destroy(trajectory);
+        }
+        if (player != null)
+        {
+            Destroy(player);
+        }
+
+        foreach(var coin in FindObjectsOfType<Moneda>())
+        {
+            Destroy(coin.gameObject);
+        }
+        foreach(var enemy in FindObjectsOfType<Enemigo>())
+        {
+            Destroy(enemy.gameObject);
+        }
+
+        StartLevel();
     }
 
     private void OnEnable()
@@ -53,6 +106,11 @@ public class GameManager : MonoBehaviour
     void PickCoin()
     {
         coinsPicked++;
+
+        if (coinsPicked == currentLevel.coinsCount)
+        {
+            isGameFinished = true;
+        }
     }
 
     void TriggerGameOver()
@@ -65,11 +123,11 @@ public class GameManager : MonoBehaviour
     {
         currentLevel = levels[0];
         StartLevel();
-        soundManager.PlaySound("Game Play Mx");
     }
 
     private void StartLevel()
     {
+        soundManager.PlaySound("Game Play Mx");
         trajectory = Instantiate(currentLevel.trajectory);
         fdet = trajectory.GetComponent<FuncionDeTrayectoria>();
         FuncionDeTrayectoria.maxIndex = fdet.points.Count-1;
@@ -84,6 +142,6 @@ public class GameManager : MonoBehaviour
 
     private void SpawnPlayer()
     {
-        GameObject player = Instantiate(playerPrefab, fdet.Trajectory(0), Quaternion.identity);
+        player = Instantiate(playerPrefab, fdet.Trajectory(0), Quaternion.identity);
     }
 }
